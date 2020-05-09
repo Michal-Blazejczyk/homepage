5860a86b3968fe8585b3514bc919439c72fabdbf
Michal <budzik555@interia.pl>
2020-05-09 17:23:03 +0100

    additional changes

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-alert("siema");
+//alert("siema");
+
+
+const firstname = 'Michal';
+const age = 32;
+
+console.log(
+    'Witaj Drogi Odwiedzjacy, nazywam sie ${firstname} i mam ${age} lata'
+    );
+
+const heading = document.querySelector('.main-heading--js');
+
+console.log(heading.innerHTML)
+
+heading.innerHtml = `Witaj Drogi Odwiedzjacy, nazywam sie ${firstname} i mam ${age} lata`
+console.log(heading)
